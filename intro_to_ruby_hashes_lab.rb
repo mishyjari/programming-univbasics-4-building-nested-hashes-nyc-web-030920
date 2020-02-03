580cfe0fb6b236587ec867e7c201c57b2e5f38e1
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-03 21:12:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   {railroads: {rent_in_dollars: {
-    one: nil,
+    names: nil,
     two: nil,
     three: nil,
     four: nil
