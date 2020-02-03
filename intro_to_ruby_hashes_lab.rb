abecb9ed9083a487669e98b1d6f27cada645515f
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-03 21:15:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,9 +20,9 @@ def monopoly_with_third_tier
   {railroads: {
     rent_in_dollars: {
       one_piece_owned: 25,
-      two: nil,
-      three: nil,
-      four: nil
+      two_pieces_owned: 50,
+      three_pieces_owned: 100,
+      four_pieces_owned: 200
     },
     names: {
       one: nil,
