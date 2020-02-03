4697bd6d17b3ebdbcb0e40b56d9a0cf6ef84514d
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-03 21:19:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,9 +48,9 @@ def monopoly_with_fourth_tier
     },
     names: {
       reading_railroad: {mortgage_value: '$100'},
-      pennsylvania_railroad: {},
-      b_and_o_railroad: {},
-      shortline_railroad: {}
+      pennsylvania_railroad: {mortgage_value: '$100'},
+      b_and_o_railroad: {mortgage_value: '$100'},
+      shortline_railroad: {mortgage_value: '$100'}
     }
     
   }}
